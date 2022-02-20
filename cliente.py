233af3d7d90bb9e07d822332475e9d5dbffa60ef
Rodrigo Coutelo <rodrigo@coutelo.me>
2022-02-20 00:49:17 -0300

    simplificando as coisas

--- cliente.py
+++ cliente.py
@@ -1,5 +1,5 @@
 import random
-from locacao import Locacao
+import datetime as dt
 from loja import Loja
 
 
@@ -8,32 +8,28 @@ class Cliente(object):
   def __init__(self, nome):
     self._id = random.randint(1,999) #simulando a geraçao de id único para loja
     self.nome = nome
+    self.hora_aluguel = None
+    self.quantidade = None
+    self.modalidade = None
     print(f"Classe Cliente | Metodo Construtor | Cria instância de cliente\n{self}\n")
 
   def __repr__(self)->str:
-    return f"[Cliente -> id:{self._id}|nome:{self.nome}]"
+    return f"[Cliente -> id:{self._id}|nome:{self.nome}|hora alguel:{self.hora_aluguel}]"
 
-  def alugar(self, quantidade, modalidade, loja)->Locacao:
+  def alugar(self, quantidade, modalidade, loja):
     if not isinstance(loja, Loja):
       raise ValueError(f'Erro | Classe Cliente | Metodo alugar | parametro loja inválido. Tipo {type(loja)}')
+
     print(f"Classe Cliente | Metodo alugar | Cliente {self.nome} solicita o aluguel de {quantidade} de bicicletas na modalidade {modalidade} a loja {loja.nome}\n{self}\n")
-    locacao = Locacao(self, loja, quantidade, modalidade)
-    return loja.locar(locacao)
+    self.hora_aluguel = dt.datetime.now()
+    self.quantidade = quantidade
+    return loja.locar(self)
 
-  def devolver(self, locacao:Locacao)->float:
-    if not isinstance(locacao,Locacao):
-     raise ValueError(f'Erro | Classe Cliente  | Metodo devolver - parametro locacao inválido. Tipo {type(locacao)}')
-
-    if locacao.cliente != self:
-      raise Exception(f"Erro | Classe Cliente  | Metodo devolver | Essa locação não pertence ao cliente {self.nome}")
-
-    print(f"Classe Cliente | Metodo devolver | Cliente {self.nome}  devolve os produtos a {locacao.loja.nome}\n{locacao}\n")
-
-    locacao = locacao.loja.receber(locacao)
-
-    print(f"Classe Cliente | Metodo devolver | Cliente {self.nome}  recebe da {locacao.loja.nome} o valor total da locação R${locacao.valor_locacao}\n{locacao}\n")
-
-    return locacao.valor_locacao
+  def devolver(self, loja)->float:
+    print(f"Classe Cliente | Metodo devolver | Cliente {self.nome}  devolve os produtos a {loja.nome}\n{self}\n")
+    valor_locacao = loja.receber(self)
+    print(f"Classe Cliente | Metodo devolver | Cliente {self.nome}  recebe da {loja.nome} o valor total da locação R${valor_locacao}\n{self}\n")
+    return valor_locacao
 
   def ler_estoque(self, loja)->int:
     if not isinstance(loja, Loja):
--- loja.py
+++ loja.py
@@ -1,6 +1,7 @@
 import random
-import locacao as loc
-from locacao import Locacao
+import datetime as dt
+
+from cliente import Cliente
 
 
 class Loja(object):
@@ -9,6 +10,9 @@ class Loja(object):
     self._id = random.randint(1,999) #simulando a geraçao de id único para loja
     self.nome = nome
     self._estoque = estoque
+    self._inicio = None
+    self._fim = None
+
     if type(estoque) != int:
       raise ValueError(f"Erro | Classe Loja | Metodo Construtor | parametro estoque inválido. Tipo {type(estoque)}")
 
@@ -21,48 +25,40 @@ class Loja(object):
     print(f"Classe Loja | Metodo mostrar_estoque | Loja {self.nome} mostra o estoque ao Cliente\n{self}\n")
     return self._estoque
 
-  def locar(self, locacao:Locacao)->Locacao:
-    if locacao.quantidade > self._estoque:
+  def locar(self, cliente) -> Cliente:
+    if cliente.quantidade > self._estoque:
       raise Exception(f"Erro | Classe Loja | Metodo locar | Loja {self.nome} com estoque indisponível")
+    if not isinstance(cliente,Cliente):
+      raise Exception(f"Erro | Classe Loja | Metodo locar | parametro loja inválido. Tipo {type(cliente)}")
+    self._estoque -= cliente.quantidade
+    self._inicio = cliente.hora_aluguel
+    print(f"Classe Loja | Metodo locar | Loja {self.nome} recebe pedido de locação do Cliente {cliente.nome}\n")
+    return cliente
 
-    self._estoque -= locacao.quantidade
-    locacao.abrir_locacao()
-    print(f"Classe Loja | Metodo locar | Loja {self.nome} recebe pedido de locação do Cliente {locacao.cliente.nome}\n{locacao}\n")
-    return locacao
+  def receber(self, cliente:Cliente) -> float:
+    print(f"Classe Loja | Metodo receber | Loja {self.nome} recebeu os produtos de volta do Cliente {cliente.nome} e vai processar os cálculos\n")
+    self._estoque += cliente.quantidade
+    self._fim = cliente.hora_aluguel
+    valor_locacao = self.calcula_valor_locacao(cliente)
+    return valor_locacao
 
-  def receber(self, locacao:Locacao)->Locacao:
-    if not isinstance(locacao, Locacao):
-      raise ValueError(f'Erro | Classe Loja |Metodo receber | parametro locacao inválido. Tipo {type(locacao)}')
-    if locacao.status == None:
-      raise Exception(f"Erro | Classe Loja | Metodo receber | Locação não aberta. Não pode ser devolvida.")
-    if locacao.status == loc._FECHADA:
-      raise Exception (f"Erro | Classe Loja | Metodo receber | Locação encerrada. Não pode ser mais devolvida.")
+  def calcula_valor_locacao(self, cliente:Cliente)->float:
+    print(f"Classe Loja | Metodo calcula_valor_locacao | Loja {self.nome} calcula o valor da locação\n")
+    duracao_locacao = self.calcula_tempo_locacao()
+    if cliente.modalidade == "hora":
+      valor_locacao = cliente.duracao_locacao * 5 * cliente.quantidade
+    elif cliente.modalidade == "dia":
+      valor_locacao = cliente.duracao_locacao * 25 * cliente.quantidade
+    elif cliente.modalidade == "semana":
+       valor_locacao = cliente.duracao_locacao * 100 * cliente.quantidade
 
-    print(f"Classe Loja | Metodo receber | Loja {self.nome} recebeu os produtos de volta do Cliente {locacao.cliente.nome} e vai processar os cálculos\n{locacao}\n")
-
-    self._estoque += locacao.quantidade
-    valor_locacao = self.calcula_valor_locacao(locacao)
-    locacao.valor_locacao = valor_locacao
-    locacao.fechar_locacao()
-    return locacao
-
-  def calcula_valor_locacao(self, locacao:Locacao)->float:
-    print(f"Classe Loja | Metodo calcula_valor_locacao | Loja {self.nome} calcula o valor da locação\n{locacao}\n")
-    locacao.calcula_tempo_locacao()
-    if locacao.modalidade == loc._HORA:
-      valor_locacao = locacao.duracao_locacao * 5 * locacao.quantidade
-    elif locacao.modalidade == loc._DIA:
-      valor_locacao = locacao.duracao_locacao * 25 * locacao.quantidade
-    elif locacao.modalidade == loc._SEMANA:
-       valor_locacao = locacao.duracao_locacao * 100 * locacao.quantidade
-
-    if self.is_elegivel_desconto(locacao):
+    if self.is_elegivel_desconto(cliente):
       valor_locacao = 0.7 * valor_locacao
-    print(f"Classe Loja | Metodo is_elegivel_desconto | Loja {self.nome} checa se o produto é elegivel de desconto | Retorno {self.is_elegivel_desconto(locacao)}\n{locacao}\n")
+    print(f"Classe Loja | Metodo is_elegivel_desconto | Loja {self.nome} checa se o produto é elegivel de desconto | Retorno {self.is_elegivel_desconto(cliente)}\n")
     return valor_locacao
 
 
-  def is_elegivel_desconto(self, locacao:Locacao)->bool:
-    if (locacao.quantidade >= 3):
+  def is_elegivel_desconto(self, cliente:Cliente)->bool:
+    if (cliente.quantidade >= 3):
       return True
     return False
